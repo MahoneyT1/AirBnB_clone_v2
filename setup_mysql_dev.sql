50cc070e5ff421cde2a36644cce6f2cdc087d386
MahoneyT1 <ifeanyitech15@gmail.com>
2024-05-28 22:21:43 -0700

    db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,7 +8,7 @@
 -- If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
 
 CREATE DATABASE if NOT EXISTS hbnb_dev_db ;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH authentication_plugin BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-GRANT SELECT PRIVILEGES ON performance_schema. TO 'hbnb_dev'@'localhost' WITH GRANT OPTION
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
