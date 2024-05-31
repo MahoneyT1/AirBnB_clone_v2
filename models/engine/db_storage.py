ff16924a82ba069ebc31800d4c9298c00cca0e43
MahoneyT1 <ifeanyitech15@gmail.com>
2024-05-31 11:28:07 +0100

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,14 +77,14 @@ class DBStorage:
 
             for obj in result:
                 key = f"{cls.__class__.__name__}.{obj.id}"
-                new_object[key] = obj
+                new_object[key] = obj.to_dict()
         else:
             for class_name, class_obj in self.classes.items():
                 result = self.__session.query(class_obj)
                 print(f"Query result: {result}")
                 for obj in result:
                     key = f"{class_obj.__name__}.{obj.id}"
-                    new_object[key] = obj
+                    new_object[key] = obj.to_dict()
         return new_object
     def new(self, obj):
         """
