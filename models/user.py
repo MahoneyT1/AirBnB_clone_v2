5e8800b863c2e15947deb8cbf85866c41ba602f2
MahoneyT1 <ifeanyitech15@gmail.com>
2024-06-01 13:54:44 +0100

    upgrade

--- models/user.py
+++ models/user.py
@@ -12,4 +12,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    places = relationship('place', backref='user', cascade="all, delete-orphan")
+    places = relationship('place', backref='places', cascade="all, delete-orphan")
