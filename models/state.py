3a311e0fb42da14f7b2dc619db9b730aa6188d3f
MahoneyT1 <ifeanyitech15@gmail.com>
2024-06-05 15:04:08 +0100

    trying to fix the cities method

--- models/state.py
+++ models/state.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    name = Column(String(128), ForeignKey('states.id'), nullable=False)
     cities = relationship('City', backref='state', cascade="all, delete, delete-orphan")
 
     @property
