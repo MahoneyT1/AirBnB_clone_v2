162191e7f94efb4360bb146e7ecfaa00dc365184
MahoneyT1 <ifeanyitech15@gmail.com>
2024-06-16 20:07:07 +0100

    added views in

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 from api.v1.views import app_views
+from flask import jsonify
 
-@app_views.route('/status')
-def app_view():
-    return {'status':'ok'}
+@app_views.route('/status', methods=['GET'])
+def status():
+    return jsonify({'status':'ok'})
