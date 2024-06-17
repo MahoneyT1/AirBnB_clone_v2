31f0ede14beb648ddb80d7e273b78d7ca1784fb3
MahoneyT1 <ifeanyitech15@gmail.com>
2024-06-17 13:32:17 +0100

    fixed the flask app

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-
+# call the instance of blueprint with app_views as an arg
 app_views = Blueprint('app_views', __name__)
 from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+# create a custom route
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({'status':'ok'})
