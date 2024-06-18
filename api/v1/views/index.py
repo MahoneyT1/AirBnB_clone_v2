15086d350a266dbbfc0a31afd8500bdb24ba57d6
MahoneyT1 <ifeanyitech15@gmail.com>
2024-06-18 10:30:36 +0100

    updated pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 """
 Flask route that returns json status response
-""" 
+"""
 
 from api.v1.views import app_views
 from flask import jsonify
 
 # create a custom route
+
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """The status route that confirms
     if the server is okay
     """
-    return jsonify({'status':'ok'})
+    return jsonify({'status': 'ok'})
+
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
     """Function to return the count of all class objects
     """
-    from  models import storage
+
+    from models import storage
     from models import user, place, review, state, city, amenity
 
     # create an object and map the count of each instance in
