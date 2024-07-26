f10a3053ad40904aa5ec3a39527ff70d61103b04
MahoneyT1 <ifeanyitech15@gmail.com>
2024-07-26 11:42:58 +0100

    python is cool

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
-"""Start a Flask Application
+"""
+start Flask application
 """
 
 from flask import Flask
-from markupsafe import escape
-
-
-# An instance of flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def display():
-    """Displays Hello HBNB!"""
-    return escape('Hello HBNB!')
 
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
+# from flask import Flask
+# from markupsafe import escape
+
+
+# # An instance of flask
+# app = Flask(__name__)
+
+# @app.route('/', strict_slashes=False)
+# def display():
+#     """Displays Hello HBNB!"""
+#     return escape('Hello HBNB!')
+
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
