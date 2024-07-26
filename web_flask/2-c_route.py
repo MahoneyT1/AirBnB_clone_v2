c800050d0f9df12bbe58ba6b29fb4baf317d3c88
MahoneyT1 <ifeanyitech15@gmail.com>
2024-07-26 12:08:16 +0100

    python is cool

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,29 +4,25 @@ Your web application must be listening on 0.0.0.0,
 port 5000
 """
 
-
 from flask import Flask, render_template
 
-# An instance of flask
+
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'],
-            strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def display():
     """Displays “Hello HBNB! """
     return 'Hello HBNB!'
 
-@app.route("/hbnb", methods=['GET'],
-                strict_slashes=False)
 
+@app.route('/hbnb', strict_slashes=False)
 def display_two():
     """ / hbnb: display HBNB"""
     return 'HBNB'
 
-@app.route("/c/<text>", methods=['GET'],
-                    strict_slashes=False)
 
+@app.route('/c/<text>', strict_slashes=False)
 def display_third(text):
     """Display C"""
     if text:
