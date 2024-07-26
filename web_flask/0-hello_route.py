272981ff00cca3a23b94a63b8fb46a27d2cb430b
MahoneyT1 <ifeanyitech15@gmail.com>
2024-07-26 11:56:38 +0100

    fixed pycodestyle'

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,29 +4,17 @@ start Flask application
 """
 
 from flask import Flask
+from markupsafe import escape
+
+
+# An instance of flask
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def display():
+    """Displays Hello HBNB!"""
+    return escape('Hello HBNB!')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-# from flask import Flask
-# from markupsafe import escape
-
-
-# # An instance of flask
-# app = Flask(__name__)
-
-# @app.route('/', strict_slashes=False)
-# def display():
-#     """Displays Hello HBNB!"""
-#     return escape('Hello HBNB!')
-
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
