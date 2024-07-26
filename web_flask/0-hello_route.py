c15c280b2f8b650d9e1f385be73b0fcccef85bb9
MahoneyT1 <ifeanyitech15@gmail.com>
2024-07-26 11:38:08 +0100

    python is cool

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application
-Your web application must be listening on 0.0.0.0,
-port 5000
+"""Start a Flask Application
 """
-
 
 from flask import Flask, make_response, jsonify
 from markupsafe import escape
@@ -12,11 +9,10 @@ from markupsafe import escape
 # An instance of flask
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def display():
-    """Displays (Hello HBNB!)
-    """
-    return escape("Hello HBNB!")
+    """Displays Hello HBNB!"""
+    return escape('Hello HBNB!')
 
 
 if __name__ == "__main__":
