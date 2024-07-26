39fc743dc439436465c6f7d1025a2dd221627166
MahoneyT1 <ifeanyitech15@gmail.com>
2024-07-26 12:17:21 +0100

    Python is

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,37 +8,29 @@ from flask import Flask, render_template
 # An instance of flask
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'],
-           strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def display():
     """Displays “Hello HBNB! """
     return 'Hello HBNB!'
 
-@app.route("/hbnb", methods=['GET'],
-                strict_slashes=False)
 
+@app.route('/hbnb', strict_slashes=False)
 def display_two():
     """ / hbnb: display HBNB"""
     return 'HBNB'
 
-@app.route("/c/<text>", methods=['GET'],
-                    strict_slashes=False)
 
+@app.route('/c/<text>', strict_slashes=False)
 def display_third(text):
     """Display C"""
     if text:
         text = text.replace('_', ' ')
-        return """
-                <HTML><head><body> %% {{text}} %% </body></head></HTML>
-                """
+        return f'C {text}'
 
-@app.route('/python/', methods=['GET'], 
-                    strict_slashes=False)
 
-@app.route('/python/<text>', methods=['GET'], 
-                        strict_slashes=False)
-
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def display_python(text='is cool'):
     """Display 'Python ', followed by the value of
     the text variable (replace underscores with spaces).
